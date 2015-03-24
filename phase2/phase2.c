302fb1295f8cab4d65d950bbba8414604d8de976
N R Callahan <nicholas.r.callahan@gmail.com>
2015-03-24 13:37:52 -0700

    Starts new user level processes. Does no checks though

--- phase2/phase2.c
+++ phase2/phase2.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <usloss.h>
+#include <usyscall.h>
+#include <libuser.h>
 #include <phase1.h>
 #include <phase2.h>
 
@@ -15,73 +17,96 @@
  *  Clock Device Driver         Incomplete (total)
  *  Sys_Spawn                   Incomplete (total)
  *  Mailbox System Calls        Incomplete (total)
+ *  User PCB Struct             Incomplete (total)
  */
+#define DEBUG   1
+#define SET_USERMODE USLOSS_PsrSet(USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE)
+
+typedef struct {
+    int current_status;
+    int parent_pid;
+    int return_status;
+    //Need list of children
+} User_PCB;
+
+
+/* -----------------------Global Variables--------------------------------- */
+P1_Semaphore spawning;
+char *launch_name;
+int(*launch_func)(void *);
+void *launch_arg;
+int parent_pid;
+/* ----------------------Function Defnitions------------------------------- */
+int P2_Spawn(char *name, int(*func)(void *), void *arg, int stackSize, int priority);
+int P2_Wait(int *status);
+void set_up_sysvec();
+void checkMode();
+int user_launch(void *arg);
+/* -----------------Sys Call Function Definitions-------------------------- */
+void Spawn_Syscall(USLOSS_Sysargs *saPtr);
+void Terminate_Syscall(USLOSS_Sysargs *saPtr);
+
 int P2_Startup(void *arg) {
-
+    set_up_sysvec();
+    P2_Spawn("P3_Startup",P3_Startup,NULL,4*USLOSS_MIN_STACK,3);
+    return 0;
 }
-
-
-
-///////////////////////////////////////////////////////
-//                 2.1   Clock Driver                //
-///////////////////////////////////////////////////////
-
-int P2_Sleep(int seconds) {
-
+/* ------------------------Helper Functions-------------------------------- */
+void set_up_sysvec() {
+    syscalls_vec[SYS_SPAWN] = Spawn_Syscall;
+    syscalls_vec[SYS_TERMINATE] = Terminate_Syscall;
 }
-
-
-///////////////////////////////////////////////////////
-//                 2.2  Terminal Driver              //
-///////////////////////////////////////////////////////
-
-int P2_TermRead(int unit, int size, char *buffer) {
-
+void checkMode() {
+    if((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0 ) {
+        USLOSS_Console("ERROR: Not in kernel mode.");
+        USLOSS_Halt(1);
+    }
 }
-
-int P2_TermWrite(int unit, int size, char *text) {
-
+/* -------------------------- Functions ----------------------------------- */
+void Spawn_Syscall(USLOSS_Sysargs *saPtr) {
+    if(DEBUG == 1) USLOSS_Console("Spawn_Syscall(): Executing\n");
+    long error=-1;
+//--Get info out of SysArgs Struct
+    char *name = (char *)(saPtr->arg5);
+    int(*func)(void *) = saPtr->arg1;
+    void *arg = saPtr->arg2;
+    int stackSize = (int)((long)(saPtr->arg3));
+    int priority = (int)((long)saPtr->arg4);
+//--Spawn the process
+    long pid = (long)P2_Spawn(name,func,arg,stackSize,priority);
+//--Return based on results
+    if(DEBUG == 1) USLOSS_Console("Spawn_Syscall(): Spawn Process %s return with %d\n",name,pid);
+    if(pid < 0) {
+        saPtr->arg1=(void *)error;
+        saPtr->arg4=(void *)error;
+    }
+    else {
+        saPtr->arg1=(void *)pid;
+        saPtr->arg4=(void *)(error+1);
+    }
 }
-
-///////////////////////////////////////////////////////
-//                 2.3   Disk Driver                 //
-///////////////////////////////////////////////////////
-
-int P2_DiskRead(int unit, int track, int first, int sectors, void *buffer) {
-
+void Terminate_Syscall(USLOSS_Sysargs *saPtr) {
+    if(DEBUG == 1) USLOSS_Console("Term_SysCall(): Executing\n");
+    if(DEBUG == 1) USLOSS_Console("Term_Syscall(): Process exited with: %d\n",(int)((long)(saPtr->arg1)));
+    P1_Quit((int)((long)saPtr->arg1));
 }
+int P2_Spawn(char *name, int(*func)(void *), void *arg, int stackSize, int priority) {
+    if(DEBUG == 1) USLOSS_Console("P2_Spawn(): Executing\n");
+    checkMode();
+    launch_name = name;
+    launch_func = func;
+    launch_arg = arg;
+    parent_pid = P1_GetPID();
 
-int P2_DiskWrite(int unit, int track, int first, int sectors, void *buffer) {
-
+    return P1_Fork(name,user_launch,arg,stackSize,priority);
 }
-
-int P2_DiskSize(int unit, int *sector, int *track, int*disk) {
-
+int user_launch(void *arg) {
+    SET_USERMODE;
+//--Create user pcb
+    User_PCB new_proc;
+    new_proc.parent_pid=parent_pid;
+    USLOSS_Console("Launching Startup!\n");
+    int return_val = launch_func(arg);
+    Sys_Terminate(return_val);
+    return 0;
 }
-
-///////////////////////////////////////////////////////
-//                 3.x  System Calls                 //
-///////////////////////////////////////////////////////
-
-//3.1   Sys_TermRead(syscall 1)
-//3.2   Sys_TermWrite(syscall 2)
-//3.3   Sys_Spawn(syscall 3)
-//3.4   Sys_Wait(syscall 4)
-//3.5   Sys_Terminate(syscall 5)
-//3.6   Sys_MboxCreate(syscall 6)
-//3.7   Sys_MboxRelease(syscall 7)
-//3.8   Sys_MboxSend(syscall 8)
-//3.9   Sys_MboxReceive(syscall 9)
-//3.10  Sys_MboxCondSend(syscall 10)
-//3.11  Sys_MboxCondReceive(syscall 11)
-//3.12  Sys_Sleep(syscall 12)
-//3.13  Sys_DiskRead(syscall 13)
-//3.14  Sys_DiskWrite(syscall 14)
-//3.15  Sys_DiskSize(syscall 15)
-//3.16  Sys_SemCreate(syscall 16)
-//3.17  Sys_SemP(syscall 17)
-//3.18  Sys_SemV(syscall 18)
-//3.19  Sys_SemFree(syscall 19)
-//3.20  Sys_GetTimeofDay(syscall 20)
-//3.21  Sys_CPUTime(syscall 21)
-//3.22  Sys_GetPID(syscall 22)
